50fddf1e9aafc2062decda304225142bf9692498
Iruesiri Onoro <iruesirio@gmail.com>
2023-12-19 14:30:07 -0330

    outputs and variables added

--- main.tf
+++ main.tf
@@ -37,51 +37,61 @@ resource "aws_route_table" "mtc_public_rt" {
 }
 
 resource "aws_route" "default_route" {
-  route_table_id = aws_route_table.mtc_public_rt.id
+  route_table_id         = aws_route_table.mtc_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
 }
 
 resource "aws_route_table_association" "mtc_public_assoc" {
-  subnet_id = aws_subnet.mtc_public_subnet.id
+  subnet_id      = aws_subnet.mtc_public_subnet.id
   route_table_id = aws_route_table.mtc_public_rt.id
 }
 
-resource "aws_security_group" "mtc_sg"{
-    name = "dev_sg"
-    description = "dev security group"
-    vpc_id = aws_vpc.mtc_vpc.id
+resource "aws_security_group" "mtc_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.mtc_vpc.id
 
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["156.57.85.18/32"]
-    }
-    egress{
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["156.57.85.18/32"]
-    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["156.57.85.18/32"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-resource "aws_key_pair" "mtc_auth"{
-      key_name = "mtckey"
-      public_key = file("~/.ssh/mtckey..pub")
-    }
-resource "aws_instance" "dev_node"{ 
-  instance_type = "t2.micro"
-  ami = data.aws_ami.server_ami.id
-  key_name = aws_key_pair.mtc_auth.id
+resource "aws_key_pair" "mtc_auth" {
+  key_name   = "mtckey"
+  public_key = file("~/.ssh/mtckey..pub")
+}
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.mtc_auth.id
   vpc_security_group_ids = [aws_security_group.mtc_sg.id]
-  subnet_id = aws_subnet.mtc_public_subnet.id
-  user_data = file("userdata.tpl")
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+  user_data              = file("userdata.tpl")
 
-  root_block_device{
+  root_block_device {
     volume_size = 10
   }
 
   tags = {
     Name = "dev-node"
   }
+
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
+      hostname = self.public_ip,
+      user = "ubuntu",
+      identityfile = "~/.ssh/mtckey"
+    })
+    interpreter = var.host_os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
+  }
+
 }
